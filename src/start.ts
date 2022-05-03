ee49a32ca8431085f147af59a223189481fc3226
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-05-02 21:30:11 -0500

    :construction: -Add router and /health-check

---
+++ src/start.ts
@@ -0,0 +1,14 @@
+import { App } from './app'
+
+try {
+  // eslint-disable-next-line no-unused-expressions
+  new App().start()
+} catch (error) {
+  console.log(error)
+  process.exit(1)
+}
+
+process.on('uncaughtException', err => {
+  console.log('uncaughtException', err)
+  process.exit(1)
+})
