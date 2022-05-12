ecfdcdf90ca95cf98211f50c755779dbca8230e7
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-05-11 19:45:19 -0500

    :space_invader: -Fix test

--- test/events/send/index/indexBusSend.test.ts
+++ test/events/send/index/indexBusSend.test.ts
@@ -1,23 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable global-require */
-
-
 import { describe, expect, test } from '@jest/globals'
-
-import { IMessageBus } from '../../../../../userService/src/shared/interfaces/messageBus'
-import { IDataBus } from '../../../../src/shared/interfaces/messageBus';
-
-const message: IMessageBus = {
-  data: {
-    id: 'sssss',
-    occurred: new Date('December 17, 1995 03:24:00'),
-    type: '',
-    attributes: {
-      index: ''
-    }
-  }
-}
-
 
 beforeEach(async () => {
 
