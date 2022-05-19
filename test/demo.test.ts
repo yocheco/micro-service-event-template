6bf5bab3e8e88e9e7609b96da826a4510b803fe8
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-05-19 18:25:05 -0500

    :recycle: -Delete demo test

--- test/demo.test.ts
+++
@@ -1,8 +0,0 @@
-import { sum } from '../src/demo'
-
-// eslint-disable-next-line no-undef
-describe('Demo case', () => {
-  test('should sum 1 + 2 return 3', () => {
-    expect(sum(1, 2)).toBe(3)
-  })
-})
