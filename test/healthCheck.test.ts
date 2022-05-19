9b4777ceaa5f8639bbd2eb1be0ae45841d851067
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-05-19 18:54:22 -0500

    :construction: -Add mock logger in health check

--- test/healthCheck.test.ts
+++ test/healthCheck.test.ts
@@ -1,22 +1,42 @@
 
 import request from 'supertest'
 import { App } from '../src/app'
+import winstonLogger from '../src/lib/WinstonLogger'
+import { mockError, mockInfo } from './shared/mockWinstonLogger'
+
+
 
 let application: App
+
+// info
+const winstonLoggerInfoSpy = jest.spyOn(winstonLogger, 'info')
+winstonLoggerInfoSpy.mockImplementation(mockInfo)
+
+// error
+const winstonLoggerErrorSpy = jest.spyOn(winstonLogger, 'error')
+winstonLoggerErrorSpy.mockImplementation(mockError)
 
 beforeEach(async () => {
   application = new App()
   await application.start()
+
+  // Clear mock legger
+  mockError.mockClear()
+  mockInfo.mockClear()
 })
 
 afterEach(async () => {
   await application.stop()
+
+  // Clear mock legger
+  mockError.mockClear()
+  mockInfo.mockClear()
 })
 
 describe('Get /health-check', () => {
   test('should return 200', async () => {
     await request(application.httpServer)
       .get('/health-check')
-      .expect(200)
+      .expect(200)    
   })
 })
