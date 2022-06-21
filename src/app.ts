21d44ab506d143316b893c537fd0ade90d12d048
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-06-21 17:21:02 -0500

    :construction: -Add service MongoDb

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+import { MongoDb } from './config/dataBase/dataBase'
 import { Env } from './config/env/env'
 import { recivers } from './recivers'
 import { Server } from './server'
+
+// MongoseDb
+const mongoDb = new MongoDb()
 
 export class App {
   server?: Server
@@ -8,6 +12,9 @@ export class App {
   async start () {
     const port = Env.PORT
     this.server = new Server(port)
+
+    // init mongo db
+    await mongoDb.start()
     // init Rmq reciver
     if (Env.ENV !== 'test') await recivers.start()
     return this.server.listen()
@@ -18,7 +25,9 @@ export class App {
   }
 
   async stop () {
-    // Index created stop
+    // stop mongo db
+    await mongoDb.stop()
+    // stop Rmq reciver
     if (Env.ENV !== 'test') await recivers.stop()
     return this.server?.stop()
   }
