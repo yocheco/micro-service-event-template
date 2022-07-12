066e95d06c98d9fbba7077730f104314455caf7e
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-07-11 21:36:22 -0500

    :construction: -Add errors in response

--- src/shared/interfaces/apiResponse.ts
+++ src/shared/interfaces/apiResponse.ts
@@ -1,8 +1,14 @@
 import { HttpStatusCode } from '../types/http.model'
 
-export interface IApiResponse<T> {
+export interface IApiResponseError{
+  message: string
+  erros?: string[]
+}
+
+export interface IApiResponse<T={}> {
   status: boolean
   code: HttpStatusCode
   message?: string
   data?: T
+  error?: IApiResponseError
 }
