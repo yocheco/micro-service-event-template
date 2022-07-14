e31e760aa9e32d1c724270aee87c516e822c98ae
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-07-13 19:46:27 -0500

    :recycle: -Refactor reciveController rmq

--- src/controllers/recive/createIndexController.ts
+++ src/controllers/recive/createIndexController.ts
@@ -1,7 +1,8 @@
 import { Iindex } from '../../models'
+import { IIndexMessage } from '../../shared/interfaces/rmq/messages/thisService/IIndexMessage'
 import { IReciveController } from '../../shared/interfaces/rmq/reciveRmqController'
 
-class CreateIndexController implements IReciveController<Iindex> {
+class CreateIndexController implements IReciveController<IIndexMessage> {
   public reciveRMQ = async ({ data }:{ data: Iindex }): Promise<boolean> => {
     // const n = Math.floor(Math.random() * 10)
 
--- src/shared/interfaces/rmq/reciveRmqController.ts
+++ src/shared/interfaces/rmq/reciveRmqController.ts
@@ -1,3 +1,5 @@
-export interface IReciveController<T>{
+import { IMessage } from './messages/Imessage'
+
+export interface IReciveController<T extends IMessage>{
   reciveRMQ: ({ data } : {data: T}) => Promise<boolean>
 }
